35af556f888de8b090585df20ed6ecb2d021ac2a
Lucas Leonardo <luccasleonardo8@gmail.com>
2024-06-07 21:16:38 -0300

    projeto-js

---
+++ script.js
@@ -0,0 +1,18 @@
+function changeImage() {
+
+    const img = document.getElementById("hp-1");
+
+    const currentSrc = img.getAttribute("src");
+
+    if (currentSrc === "imagens/hp-1.jpg") {
+        img.setAttribute("src", "imagens/hp-2.jpg");
+
+        img.setAttribute("alt", "harry potter e a câmara secreta");
+    }
+
+    else {
+        img.setAttribute("src", "imagens/hp-1.jpg");
+
+        img.setAttribute("alt", "harry potter e a pedra filosofal");
+    }
+}
